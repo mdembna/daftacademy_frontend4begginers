fab4161788d5c8918772d37b44d281cc31276786
mdembna <44849464+mdembna@users.noreply.github.com>
2018-12-07 15:27:01 +0100

    smooth scrolling effect - jQuery
    function which provides smooth scrolling effect.

--- main.js
+++ main.js
@@ -1,8 +1,25 @@
 
 $(document).ready(function(){
-    if($(window).width() <= 1024) {
-        window.addEventListener("hashchange", function() { 
-            scrollBy(0, -220)
-        })
-    };
+
+//smooth scrolling effect
+
+    let scrollLink = $(".scroll");
+    
+    scrollLink.click(function(event) {
+
+        event.preventDefault();
+
+        let scrollLength = {
+            scrollTop: $(this.hash).offset().top
+        };
+
+//additional offset for screens with navbar fixed od top of the page
+
+        if($(window).width() <= 1200) {
+            scrollLength.scrollTop -= 220
+        };
+
+        $('body, html').animate(scrollLength, 800);
+
+    });
 });
